79baa5769017f7450487c59feea87d0ce12711c7
Jaycob Duffel <jbdcoding@gmail.com>
2020-09-03 15:38:44 -0700

    Fix to ListItem to make it reusable

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { StyleSheet, StatusBar, Platform, View } from "react-native";
 import Screen from "./app/components/Screen";
+import ListItem from "./app/components/ListItem";
 import Icon from "./app/components/Icon";
 
 export default function App() {
   return (
     <Screen>
+      <ListItem title="My Title" ImageComponent={<Icon name="email" />}/>
     </Screen>
   );
 }
