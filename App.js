293b75be5a34a7a0f2cfd72ca8f8bf72a675be3c
Jaycob Duffel <jbdcoding@gmail.com>
2020-09-06 17:00:11 -0700

    Add text input

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import React from "react";
-import { StyleSheet, StatusBar, Platform} from "react-native";
-import ListingScreen from "./app/screens/ListingScreen";
+import React, { useState } from "react";
+import { StyleSheet, StatusBar, Platform, TextInput, Text } from "react-native";
+import Screen from "./app/components/Screen";
 
 export default function App() {
+  const [firstName, setFirstName] = useState("");
   return (
-    <ListingScreen/>
+    <Screen>
+      <Text>{firstName}</Text>
+      <TextInput
+        onChangeText={(text) => setFirstName(text)}
+        placeholder="First Name"
+        style={{
+          borderBottomColor: "#ccc",
+          borderBottomWidth: 1,
+        }}
+      />
+    </Screen>
   );
 }
 
