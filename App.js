3fff5ee697145d64c845a512a34ccdbddc94e080
Jaycob Duffel <jbdcoding@gmail.com>
2020-09-10 09:39:42 -0700

    Add native image select

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as ImagePicker from "expo-image-picker";
-import { StyleSheet } from "react-native";
+import * as Permissions from "expo-permissions";
+import { StyleSheet, Button, Image } from "react-native";
+
 import Screen from "./app/components/Screen";
 
 export default function App() {
+  const [imageURI, setImageURI] = useState();
+
   const requestPermission = async () => {
     const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
-    if (!granted) alert("You need to allow camera roll access to upload pictures.");
-    
+    if (!granted) {
+      alert("You need to allow camera roll access to upload pictures.");
+    }
   };
 
   useEffect(() => {
     requestPermission();
   }, []);
-  return <Screen></Screen>;
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) {
+        setImageURI(result.uri);
+      }
+    } catch (error) {
+      console.log("error reading image", error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageURI }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({});
